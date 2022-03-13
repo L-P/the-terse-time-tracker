bd2f4dc95ff7aa2f342e294c1f87705c09b1bbdd
Léo Peltier <contact@leo-peltier.fr>
2022-03-13 14:00:33 +0100

    Add running totals for work duration
    Depressing.

--- cmd/tt/cli_report.go
+++ cmd/tt/cli_report.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
+	"time"
 	"tt/internal/tt"
 	"tt/internal/util"
 )
@@ -13,6 +15,20 @@ func report(app *tt.TT, out output) error {
 	report, err := app.GetReport()
 	if err != nil {
 		return fmt.Errorf("unable to generate report: %w", err)
+	}
+
+	if out.json {
+		enc := json.NewEncoder(out.w)
+		return enc.Encode(struct { // nolint:wrapcheck
+			WorkDuration, OnCallDuration time.Duration
+			Overtime, InLieu, Taken      time.Duration
+		}{
+			report.Accumulated.WorkDuration,
+			report.Accumulated.OnCallDuration,
+			report.Accumulated.Overtime,
+			report.Accumulated.InLieu,
+			report.Accumulated.Taken,
+		})
 	}
 
 	var (
@@ -108,6 +124,8 @@ func printWeeklyReport(out output, r tt.Report, total tt.ReportEntry) {
 
 		fmt.Fprintf(&b, "  %s ", util.FormatFixedDuration(dr.WorkDuration))
 	}
+	fmt.Fprintf(&b, " %7s ", strings.TrimSuffix(r.Accumulated.WorkDuration.Truncate(time.Minute).String(), "0s"))
+	fmt.Fprintf(&b, " (%7s)", util.FormatDuration(total.WorkDuration))
 	b.WriteRune('\n')
 
 	// Daily over/under time
@@ -124,16 +142,13 @@ func printWeeklyReport(out output, r tt.Report, total tt.ReportEntry) {
 			continue
 		}
 	}
-
-	weeklyTimeSum := (r.Accumulated.Overtime + r.Accumulated.InLieu) - r.Accumulated.Taken
-	totalTimeSum := (total.Overtime + total.InLieu) - total.Taken
-
-	fmt.Fprintf(&b, " %s ", util.FormatSignedFixedDuration(weeklyTimeSum))
-	fmt.Fprintf(&b, " (%s)", util.FormatSignedFixedDuration(totalTimeSum))
-
+	weeklyDeltaSum := (r.Accumulated.Overtime + r.Accumulated.InLieu) - r.Accumulated.Taken
+	totalDeltaSum := (total.Overtime + total.InLieu) - total.Taken
+	fmt.Fprintf(&b, " %7s ", util.FormatSignedFixedDuration(weeklyDeltaSum))
+	fmt.Fprintf(&b, " (%7s)", util.FormatSignedFixedDuration(totalDeltaSum))
 	b.WriteRune('\n')
 
-	b.WriteString("   Mon.     Tue.     Wed.     Thu.     Fri.     Sat.     Sun.    Total\n\n")
+	b.WriteString("   Mon.     Tue.     Wed.     Thu.     Fri.     Sat.     Sun.     Total\n\n")
 
 	fmt.Fprint(out.w, b.String())
 }
